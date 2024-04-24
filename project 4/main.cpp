d3529492b7fbf5397edf23e6f4f2847b587a478f
Austin, Izzy <iaustin@butler.edu>
2024-04-24 17:59:28 -0400

    fixed spacing in 2-3 tree

--- project 4/main.cpp
+++
@@ -1,9 +0,0 @@
-//CS452 Project 4: Covins-20
-//Written by Anna Vadella, Noah Baker, Izzy Austin
-
-//Compile: mpicxx -o blah main.cpp
-//Run: mpirun -np 4 blah
-
-
-//i think uses InYourEye.cpp's structure from proj 1
-// no parallelism though
